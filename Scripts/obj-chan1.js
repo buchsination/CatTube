ebbe8a7eb2f7c59c70f0d2649e581155235b7831
buchsination <144206452+buchsination@users.noreply.github.com>
2023-11-08 15:16:18 +0100

    Add files via upload

---
+++ Scripts/obj-chan1.js
@@ -0,0 +1,20 @@
+/* Channel Purrfection Object */
+const channelVideo = {
+  likes: 51,
+  activeLike: false,
+  dislikes: 10,
+  activeDislike: false,
+  aufrufe: 705399,
+  chanAbo: false,
+  chanMem: false,
+  channelVideoThumb: './Thumbnails/cat-01.jpg',
+  laenge: '14:20',
+  datum: '15.08.2023',
+  titel: 'Natur ihre Schönheit und Ruhe'
+};
+
+const channelOwner = {
+  name: 'Purrfection',
+  abonnement: 30000,
+  channelPicture: './Channel-Pictures/cpic01.png',
+}
